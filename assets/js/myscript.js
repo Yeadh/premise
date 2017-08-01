2975af4de7349daefffa2040f8ba59f2d0826b1e
MD. YEADH HASAN BAPPY <yeadhapps@gmail.com>
2017-08-01 16:36:35 +0600

    Metabox hide and show

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -3,6 +3,41 @@
 	$(document).on('change','#page_template',function(){
 		var $page_template;
 		$page_template = $(this).val();
+		if($page_template == 'templates/homepage.php')
+		{
+			$('#premise_landing_page').show();	
+		}
+		else
+		{
+			$('#premise_landing_page').hide();
+		}
+		
+	});
+	$(document).ready(function(){
+		var $page_template;
+		$page_template = $('#page_template').val();
+		if($page_template == 'templates/homepage.php')
+		{
+			$('#premise_landing_page').show();	
+		}
+		else
+		{
+			$('#premise_landing_page').hide();
+		}
+		
+	
+	});
+	
+	
+	
+	
+	
+	
+	
+	
+	$(document).on('change','#page_template',function(){
+		var $page_template;
+		$page_template = $(this).val();
 		if($page_template == 'templates/about.php')
 		{
 			$('#about_landing_page').show();	
