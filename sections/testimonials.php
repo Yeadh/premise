dd425cca9bb0ccce248f00de7c41cd14fe545a4d
MD. YEADH HASAN BAPPY <yeadhapps@gmail.com>
2017-08-03 12:30:18 +0600

    testi

---
+++ sections/testimonials.php
@@ -0,0 +1,28 @@
+<section class="testimonial-section sd-section">
+	<div class="container">
+		<div class="section-header text-center">
+			<h2>Testimonials</h2>
+			<h3>Our Clients Just Love Us</h3>
+		</div><!-- .section-header -->
+		<div id="testimonial" class="owl-carousel owl-theme" style="opacity: 1; display: block;">
+			<div class="owl-wrapper-outer"><div class="owl-wrapper" style="width: 6840px; left: 0px; display: block; transition: all 0ms ease; transform: translate3d(0px, 0px, 0px); transform-origin: 570px center 0px; perspective-origin: 570px center;"><div class="owl-item" style="width: 1140px;"><div>
+				<blockquote>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+					<small>Jhon Doe</small>
+				</blockquote>
+			</div></div><div class="owl-item" style="width: 1140px;"><div>
+				<blockquote>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+					<small>Jhon Doe</small>
+				</blockquote>
+			</div></div><div class="owl-item" style="width: 1140px;"><div>
+				<blockquote>
+					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+					<small>Jhon Doe</small>
+				</blockquote>
+			</div></div></div></div>
+			
+			
+		<div class="owl-controls clickable"><div class="owl-pagination"><div class="owl-page active"><span class=""></span></div><div class="owl-page"><span class=""></span></div><div class="owl-page"><span class=""></span></div></div></div></div><!-- #testimonial -->
+	</div><!-- .container -->
+</section>
